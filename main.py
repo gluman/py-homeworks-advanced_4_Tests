724f7990799d4caa13588e423b3c0f9595e5ddcd
Glumov Andrey <gluman@yandex.ru>
2023-03-27 19:21:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+def remove_foreign_countries():
+    for items in geo_logs:
+        values = items.values()
+        for item in values:
+            if 'Россия' not in item:
+                geo_logs.remove(items)
+                remove_foreign_countries()
+
+remove_foreign_countries()
+print(geo_logs)
+
+
+
